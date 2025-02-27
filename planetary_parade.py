c1b82d28a584bc14aea22504a78c109ed0f34a68
trice <rtphokie@gmail.com>
2025-02-26 19:21:52 -0500

    refactored more out of main

--- planetary_parade.py
+++ planetary_parade.py
@@ -16,42 +16,13 @@ pd.set_option('display.max_rows', None)  # Show all columns
 pd.set_option('display.width', 1000)  # Set wide display width
 
 
-def main(minutes=30):
-    raleigh_topo = wgs84.latlon(37.2710 * N, 79.9414 * W, elevation_m=100)
-    raleigh_vector_sum = earth + raleigh_topo
+def main(start_year=2000, end_year=2050, minutes=30, lat=32.27, lon=-79.94, elevation_m=100):
+    t0 = ts.utc(start_year-1, 12, 31)  # subtract one from end year to ensure sunrise on Jan 1 is caught
+    t1 = ts.utc(end_year+1, 1, 1)  # add one to start year to ensure sunset on Dec 31 is caught
 
-    t0 = ts.utc(2000, 1, 1)
-    t1 = ts.utc(2051, 1, 1)
-    data = {}
-    visible_planet_list = ['Mercury', 'Venus', 'Mars', 'Jupiter Barycenter', 'Saturn Barycenter', 'Moon']
-    planet_list = visible_planet_list + ['Uranus Barycenter', 'Neptune Barycenter']
+    df, events = build_dataframe(elevation_m, lat, lon, minutes, t0, t1)
 
-    # find sunrise/set, points one hour before sunrise and after sunset
-    t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, raleigh_topo))
-    for ti, yi in zip(t, y):
-        calculate_sunriseset_dawndusk(data, minutes, ti, yi)
-
-    for body in planet_list:
-        planet = body.replace(' Barycenter', '')
-        t_risings, _ = almanac.find_risings(raleigh_vector_sum, eph[body], t0, t1)
-        t_settings, _ = almanac.find_settings(raleigh_vector_sum, eph[body], t0, t1)
-        for t_rise, t_set in zip(t_risings, t_settings):
-            calculate_planet_riseset(data, planet, t_rise, t_set)
-
-    # first and last dates are missing sunrise and sunset because of the timzone offset, remove them to prevent errors
-    del (data[min(data.keys())])
-    del (data[max(data.keys())])
-    events = ['morning', 'evening', 'sunrise', 'sunset']
-    for day, v in data.items():
-        for event in events:
-            dt = data[day][f'{event}_dt']
-            t = ts.from_datetime(dt)
-            for body in planet_list:
-                calculate_altitude(body, data, day, event, raleigh_vector_sum, t)
-
-    df = pd.DataFrame.from_dict(data, orient='index')
-
-    # create bunches of dataframes to slice and dice this data by time of day (sunrise/set, morning/evening) and
+    # break big dataframe apart by time of day (sunrise/set, morning/evening) as well as
     # groupings of bodys (all, just planets, just visible planets and the moon, just visible planets)
     df_filter_all = {}
     df_filter_planets = {}
@@ -76,6 +47,36 @@ def main(minutes=30):
     print(df1[df1.above_horizon >= 7])
     print("days with all 5 visible planets above horizon in the evening")
     print(df1[df1.above_treeline >= 5])
+
+
+def build_dataframe(elevation_m, lat, lon, minutes, t0, t1):
+    raleigh_topo = wgs84.latlon(lat, lon, elevation_m=elevation_m)
+    raleigh_vector_sum = earth + raleigh_topo
+    data = {}
+    visible_planet_list = ['Mercury', 'Venus', 'Mars', 'Jupiter Barycenter', 'Saturn Barycenter', 'Moon']
+    planet_list = visible_planet_list + ['Uranus Barycenter', 'Neptune Barycenter']
+    # find sunrise/set, points one hour before sunrise and after sunset
+    t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, raleigh_topo))
+    for ti, yi in zip(t, y):
+        calculate_sunriseset_dawndusk(data, minutes, ti, yi)
+    for body in planet_list:
+        planet = body.replace(' Barycenter', '')
+        t_risings, _ = almanac.find_risings(raleigh_vector_sum, eph[body], t0, t1)
+        t_settings, _ = almanac.find_settings(raleigh_vector_sum, eph[body], t0, t1)
+        for t_rise, t_set in zip(t_risings, t_settings):
+            calculate_planet_riseset(data, planet, t_rise, t_set)
+    # first and last dates are missing sunrise and sunset because of the timzone offset, remove them to prevent errors
+    del (data[min(data.keys())])
+    del (data[max(data.keys())])
+    events = ['morning', 'evening', 'sunrise', 'sunset']
+    for day, v in data.items():
+        for event in events:
+            dt = data[day][f'{event}_dt']
+            t = ts.from_datetime(dt)
+            for body in planet_list:
+                calculate_altitude(body, data, day, event, raleigh_vector_sum, t)
+    df = pd.DataFrame.from_dict(data, orient='index')
+    return df, events
 
 
 def calculate_altitude(body, data, day, event, raleigh_vector_sum, t):
